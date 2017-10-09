8f2dba93d017d8226c9b2c2ada6b6ab6e2ac012a
SYSTECdev <31071090+SYSTECdev@users.noreply.github.com>
2017-10-09 11:03:01 +0800

    Update CordovaHttpPlugin.h

--- src/ios/CordovaHttpPlugin.h
+++ src/ios/CordovaHttpPlugin.h
@@ -8,6 +8,7 @@
 - (void)acceptAllCerts:(CDVInvokedUrlCommand*)command;
 - (void)validateDomainName:(CDVInvokedUrlCommand*)command;
 - (void)post:(CDVInvokedUrlCommand*)command;
+- (void)postRaw:(CDVInvokedUrlCommand*)command;
 - (void)get:(CDVInvokedUrlCommand*)command;
 - (void)uploadFile:(CDVInvokedUrlCommand*)command;
 - (void)downloadFile:(CDVInvokedUrlCommand*)command;
